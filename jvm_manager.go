ecca2f14a8d630069dc9257b0e896a7e5ea5b62a
Nayla Hanegan <naylahanegan@gmail.com>
2024-10-04 15:36:26 -0400

    new bootstrapper

--- jvm_manager.go
+++ jvm_manager.go
@@ -16,218 +16,179 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  **/
 
- package main
+package main
 
- import (
-	 "errors"
-	 "fmt"
-	 "io/fs"
-	 "os"
-	 "path"
-	 "path/filepath"
-	 "runtime"
-	 "slices"
- )
- 
- var (
-	 ErrFailedDetermineOs  = errors.New("failed to determine os/arch")
-	 ErrNoJavaForOs        = errors.New("no java found for this os")
-	 ErrNoJavaVersionForOs = errors.New("java version for this os doesn't include the required component")
- )
- 
- type JvmManager struct {
-	 cachedMainManifest    *MainJavaManifest
-	 cachedVersionManifest *JavaManifest
- 
-	 launcherManifest LauncherJavaManifest
-	 os               string
-	 bSettings        *BootstrapSettings
-	 legacyPath       string
- }
- 
- func GetJvmManager(bs *BootstrapSettings, launcherManifest LauncherJavaManifest) (*JvmManager, error) {
-	 os, err := detectOS()
-	 if err != nil {
-		 return nil, err
-	 }
- 
-	 jvmManager := &JvmManager{
-		 launcherManifest: launcherManifest,
-		 bSettings:        bs,
-		 os:               os,
-	 }
- 
-	 mainManifest, err := GetOrCached[MainJavaManifest](
-		 bs,
-		 filepath.Join(bs.LauncherPath, ".cache", "main_java_manifest2.json"),
-		 launcherManifest.ManifestURL,
-	 )
-	 if err != nil {
-		 return nil, err
-	 }
- 
-	 jvmManager.cachedMainManifest = mainManifest
- 
-	 versions, ok := (*jvmManager.cachedMainManifest)[os]
-	 if !ok {
-		 return nil, ErrNoJavaForOs
-	 }
- 
-	 componentKeys := []string{launcherManifest.Component, launcherManifest.ComponentLegacy}
-	 var versionManifest *JavaManifest
- 
-	 for _, component := range componentKeys {
-		 version, ok := versions[component]
-		 if ok {
-			 versionManifest, err = GetOrCached[JavaManifest](
-				 bs,
-				 filepath.Join(bs.LauncherPath, ".cache", "java_"+os+"_"+component+".json"),
-				 version.ManifestURL, // Adjusted to use a hypothetical field name
-			 )
-			 if err == nil {
-				 break
-			 }
-		 }
-	 }
- 
-	 if versionManifest == nil {
-		 return nil, ErrNoJavaVersionForOs
-	 }
- 
-	 jvmManager.cachedVersionManifest = versionManifest
-	 jvmManager.legacyPath = jvmManager.GetPathLegacy()
- 
-	 return jvmManager, nil
- }
- 
- func detectOS() (string, error) {
-	 os := runtime.GOOS
-	 arch := runtime.GOARCH
-	 switch os {
-	 case "linux":
-		 if arch == "386" {
-			 return "linux-i386", nil
-		 } else if arch == "amd64" || arch == "amd64p32" {
-			 return "linux", nil
-		 }
-	 case "darwin":
-		 if arch == "arm64" {
-			 return "mac-os-arm64", nil
-		 } else if arch == "amd64" {
-			 return "mac-os", nil
-		 }
-	 case "windows":
-		 switch arch {
-		 case "386":
-			 return "windows-x86", nil
-		 case "amd64", "amd64p32":
-			 return "windows-x64", nil
-		 case "arm64":
-			 return "windows-arm64", nil
-		 }
-	 }
-	 return "", ErrFailedDetermineOs
- }
- 
- func (m *JvmManager) GetPath() string {
-	 return path.Join(m.bSettings.LauncherPath, "runtime", m.launcherManifest.Component, m.os)
- }
- 
- func (m *JvmManager) GetPathLegacy() string {
-	 return path.Join(m.bSettings.LauncherPath, "runtime", m.launcherManifest.ComponentLegacy, m.os)
- }
- 
- func (m *JvmManager) ValidateInstallation() ([]Downloadable, error) {
-	 filesToDownload := []Downloadable{}
- 
-	 if m.launcherManifest.Component != "" {
-		 primaryFiles, err := m.validateComponent(m.GetPath(), m.launcherManifest.Component)
-		 if err != nil {
-			 return nil, err
-		 }
-		 filesToDownload = append(filesToDownload, primaryFiles...)
-	 }
- 
-	 if m.launcherManifest.ComponentLegacy != "" {
-		 legacyFiles, err := m.validateComponent(m.GetPathLegacy(), m.launcherManifest.ComponentLegacy)
-		 if err != nil {
-			 return nil, err
-		 }
-		 filesToDownload = append(filesToDownload, legacyFiles...)
-	 }
- 
-	 return filesToDownload, nil
- }
- 
- func (m *JvmManager) validateComponent(basePath, component string) ([]Downloadable, error) {
-	 filesToDownload := []Downloadable{}
-	 fileList := []string{}
- 
-	 versions, ok := (*m.cachedMainManifest)[m.os]
-	 if !ok {
-		 return nil, ErrNoJavaForOs
-	 }
- 
-	 version, ok := versions[component]
-	 if !ok {
-		 return nil, ErrNoJavaVersionForOs
-	 }
- 
-	 // Assuming the correct field is `Artifacts` instead of `Files`
-	 for k, v := range version.FilesZ { // Adjust this line based on the actual structure
-		 file := filepath.Join(basePath, k)
-		 fileList = append(fileList, file)
- 
-		 if v.Type == "directory" {
-			 err := os.MkdirAll(file, os.ModePerm)
-			 if err != nil {
-				 return nil, err
-			 }
-		 } else if v.Type == "file" {
-			 _, err := os.Stat(file)
-			 if os.IsNotExist(err) {
-				 fmt.Printf("Preparing to download Java file: %s from URL: %s\n", file, v.Downloads.Raw.Url)
-				 filesToDownload = append(filesToDownload, Downloadable{
-					 Url:        v.Downloads.Raw.Url,
-					 Path:       file,
-					 Sha1:       v.Downloads.Raw.Hash,
-					 Size:       v.Downloads.Raw.Size,
-					 Executable: v.Executable,
-				 })
-			 } else {
-				 sha1 := GetHashSha1(file)
-				 if sha1 != v.Downloads.Raw.Hash {
-					 fmt.Printf("Preparing to download Java file: %s from URL: %s (hash mismatch)\n", file, v.Downloads.Raw.Url)
-					 filesToDownload = append(filesToDownload, Downloadable{
-						 Url:        v.Downloads.Raw.Url,
-						 Path:       file,
-						 Sha1:       v.Downloads.Raw.Hash,
-						 Size:       v.Downloads.Raw.Size,
-						 Executable: v.Executable,
-					 })
-				 }
-			 }
-		 }
-	 }
- 
-	 err := filepath.Walk(basePath, func(currPath string, fi fs.FileInfo, err error) error {
-		 if err != nil {
-			 return err
-		 }
- 
-		 if fi.IsDir() {
-			 return nil
-		 }
- 
-		 if !slices.Contains(fileList, currPath) {
-			 fmt.Printf("File / dir %v should not exist. Removing it.\n", currPath)
-			 if err := os.RemoveAll(currPath); err != nil {
-				 return err
-			 }
-		 }
- 
-		 return nil
-	 })
- 
-	 return filesToDownload, err
- }
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"slices"
+)
+
+var (
+	ErrFailedDetermineOs  = errors.New("failed to determine os/arch")
+	ErrNoJavaForOs        = errors.New("no java found for this os")
+	ErrNoJavaVersionForOs = errors.New("java version for this os doesn't include the required component")
+)
+
+type JvmManager struct {
+	cachedMainManifest    *MainJavaManifest
+	cachedVersionManifest *JavaManifest
+
+	launcherManifest LauncherJavaManifest
+	os               string
+	bSettings        *BootstrapSettings
+}
+
+func GetJvmManager(bs *BootstrapSettings, launcherManifest LauncherJavaManifest) (*JvmManager, error) {
+	//#region Detecting os
+	// runtime.GOARCH = 386 amd64 amd64p32 arm arm64
+	os := runtime.GOOS
+	arch := runtime.GOARCH
+	if os == "linux" {
+		os = "linux"
+		if arch == "386" {
+			os += "-i386"
+		} else if arch != "amd64" && arch != "amd64p32" {
+			return nil, ErrFailedDetermineOs
+		}
+	} else if os == "darwin" {
+		os = "mac-os"
+		if arch == "arm64" {
+			os += "-arm64"
+		} else if arch != "amd64" {
+			return nil, ErrFailedDetermineOs
+		}
+	} else if os == "windows" {
+		os = "windows"
+		if arch == "386" {
+			os += "-x86"
+		} else if arch == "amd64" || arch == "amd64p32" {
+			os += "-x64"
+		} else if arch == "arm64" {
+			os += "-arm64"
+		} else {
+			return nil, ErrFailedDetermineOs
+		}
+	} else {
+		return nil, ErrFailedDetermineOs
+	}
+	//#endregion
+
+	jvmManager := &JvmManager{
+		launcherManifest: launcherManifest,
+		bSettings:        bs,
+		os:               os,
+	}
+
+	// We load the main manifest
+	mainManifest, err := GetOrCached[MainJavaManifest](
+		bs,
+		filepath.Join(bs.LauncherPath, ".cache", "main_java_manifest.json"),
+		launcherManifest.ManifestURL,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	jvmManager.cachedMainManifest = mainManifest
+
+	// We load the manifest for the os/version
+	versions, ok := (*jvmManager.cachedMainManifest)[os]
+	if !ok {
+		return nil, ErrNoJavaForOs
+	}
+
+	version, ok := versions[launcherManifest.Component]
+	if !ok {
+		return nil, ErrNoJavaVersionForOs
+	}
+	versionManifest, err := GetOrCached[JavaManifest](
+		bs,
+		filepath.Join(bs.LauncherPath, ".cache", "java_"+os+"_"+launcherManifest.Component+".json"),
+		version[0].Manifest.Url, // @TODO: Check how versions are handled, should we DL the first or the last?
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	jvmManager.cachedVersionManifest = versionManifest
+
+	return jvmManager, nil
+}
+
+func (m *JvmManager) GetPath() string {
+	return path.Join(m.bSettings.LauncherPath, "runtime", m.launcherManifest.Component, m.os)
+}
+
+// Returns a list of files to re-download
+func (m *JvmManager) ValidateInstallation() ([]Downloadable, error) {
+	bp := m.GetPath()
+
+	filesToDownload := []Downloadable{}
+	fileList := []string{}
+
+	for k, v := range m.cachedVersionManifest.Files {
+		file := filepath.Join(bp, k)
+		fileList = append(fileList, file)
+
+		if v.Type == "directory" {
+			err := os.MkdirAll(file, os.ModePerm)
+			if err != nil {
+				return nil, err
+			}
+		} else if v.Type == "file" {
+			_, err := os.Stat(file)
+			if !os.IsNotExist(err) {
+				sha1 := GetHashSha1(file)
+				if sha1 == v.Downloads.Raw.Hash {
+					// The file exists and has the correct hash
+					// No need to redownload
+
+					// Just checking the executable flag
+					if v.Executable {
+						err := os.Chmod(file, os.ModePerm)
+						if err != nil {
+							return nil, err
+						}
+					}
+					continue
+				}
+			}
+
+			filesToDownload = append(filesToDownload, Downloadable{
+				Url:        v.Downloads.Raw.Url,
+				Path:       file,
+				Sha1:       v.Downloads.Raw.Hash,
+				Size:       v.Downloads.Raw.Size,
+				Executable: v.Executable,
+			})
+		}
+	}
+
+	// Removing the files that should not exist
+	err := filepath.Walk(bp, func(currPath string, fi fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if fi.IsDir() {
+			return nil
+		}
+
+		if !slices.Contains(fileList, currPath) {
+			fmt.Printf("File / dir %v should not exist. Removing it.\n", currPath)
+			if err := os.RemoveAll(currPath); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return filesToDownload, err
+}
